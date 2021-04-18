8ac76e250e8aa14a5c929a49bcfcb5e2d0c1e189
Aleksandrs Barkancevs <barkancev.alexander@gmail.com>
2021-04-19 01:17:42 +0300

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,5 +1,7 @@
 const fetchCountries = searchQuery => {
-  const url = `http://restcountries.eu/rest/v2/name/${searchQuery}`;
+  const query = encodeURI(searchQuery);
+  const filter = '?fields=name;capital;languages;population;flag';
+  const url = `http://restcountries.eu/rest/v2/name/${query}${filter}`;
   return fetch(url).then(response => {
     if (response.ok) return response.json();
     throw new Error(`Fetching data: ${response.status}`);
